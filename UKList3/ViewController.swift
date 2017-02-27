71515eca63a7861ec7197182d3509ffe162f6992
Naoko Hishinuma <NAOKO@naoko-no-macbook.local>
2017-02-27 22:21:39 +0900

    Initial Commit

---
+++ UKList3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UKList3
+//
+//  Created by Naoko Hishinuma on 2017/02/27.
+//  Copyright © 2017年 Naoko Hishinuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
